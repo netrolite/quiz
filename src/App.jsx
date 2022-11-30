ca15f59f57f95f16c61fbfdfe775b4306638a855
Matvey <morozovmatvej75@gmail.com>
2022-11-30 20:06:04 +0300

    bootstrap rows cols testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  
+export default class App extends React.Component {
+    render() {
+        return (
+            <main className="container-xl">
+                <div className="row row-cols-sm-2 row-cols-1">
+                    <div className="col">
+                        <div className="box"></div>
+                    </div>
+                    <div className="col">
+                        <div className="box"></div>
+                    </div>
+                    <div className="col">
+                        <div className="box"></div>
+                    </div>
+                    <div className="col">
+                        <div className="box"></div>
+                    </div>
+                    <div className="col">
+                        <div className="box"></div>
+                    </div>
+                    <div className="col">
+                        <div className="box"></div>
+                    </div>
+                </div>
+            </main>
+        )
+    }
 }
-
-export default App
