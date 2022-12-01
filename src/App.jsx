e9684327eb0ef08b40dee49915031b0c0ad6c817
Matvey <morozovmatvej75@gmail.com>
2022-12-01 17:09:04 +0300

    recieve props

--- src/App.jsx
+++ src/App.jsx
@@ -4,67 +4,10 @@ import "bootstrap/dist/css/bootstrap.min.css"
 export default class App extends React.Component {
     render() {
         return (
-            <main className="container-fluid">
-                <div
-                    className="
-                        row
-                        row-cols-1
-                        row-cols-sm-2
-                        row-cols-md-3
-                        row-cols-lg-4
-                        row-cols-xxl-6
-
-                        gx-0
-                        gx-sm-1
-                        gx-md-2
-                        gx-lg-3
-                        gx-xxl-4
-
-                        gy-1
-                        gy-sm-1
-                        gy-md-2
-                        gy-lg-3
-                        gy-xxl-4
-                    "
-                >
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                    <div className="col">
-                        <div className="box"></div>
-                    </div>
-                </div>
-            </main>
+            <>
+                <h1>{this.props.string}</h1>
+                <h1>{this.props.bool.toString()}</h1>
+            </>
         )
     }
 }
