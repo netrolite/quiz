4d9f2b7bd3ef411f7318d33610760df5bdbe90fd
Matvey <morozovmatvej75@gmail.com>
2022-12-02 19:31:51 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ export default class App extends React.Component {
     }
 
     componentDidMount() {
+        this.getFact()
+    }
+
+    getFact = () => {
         fetch("https://catfact.ninja/fact")
             .then(res => {
                 if (res.ok) return res.json();
@@ -20,7 +24,13 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <main>
+            <main className="container">
+                <button
+                    style={{marginBottom: "25px", display: "block"}}
+                    onClick={this.getFact}
+                >
+                    Get Fact
+                </button>
                 {this.state.catFact}
             </main>
         )
